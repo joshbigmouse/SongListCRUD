600f82354649031f728ee0af7b693fb3db36ea09
Joselito Bacolod <115162392+joshbigmouse@users.noreply.github.com>
2023-11-13 16:20:17 +0800

    Add files via upload

---
+++ Models/Song.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorSongList.Models
+{
+    public class Song
+    {
+        public string SongId { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public string Artist { get; set; } = null!;
+        public string Year { get; set; } = null!;
+    }
+}
